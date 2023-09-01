690495d6cbc70635d2f6d5c725a6ff7ab86470e8
Christian <purin213@hotmail.com>
2023-09-01 23:10:47 +0900

    complete the four commands & improved error handling & wroteout README

---
+++ README.md
@@ -0,0 +1,28 @@
+#File Muipulator Program.
+This is a basic file manipulator program.
+This program should be called from the command line with python3 like so below
+
+```
+python3 main.py {command}
+```
+
+##Below are all the applicable inputs and the required arguments.
+
+```
+reverse {inputPath} {outputPath}
+copy {inputPath} {outputPath}
+duplicate {inputPath} {n}
+replace {inputPath} {needle} {newString}
+```
+
+###reverse
+>reverses the input and outputs to the outputpath
+
+###copy
+>simply copy and output to the outputpath
+
+###duplicate
+>duplicates the content in the input file and duplicates within the input n times
+
+###replace
+>replaces the needle in the input with the newString
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import sys
+import shutil
 
 argList = sys.argv
+operation = argList[1]
 inputPath = argList[2]
 
-if len(argList) > 4:
-    sys.stderr.write('Error! wrong argument length, please check from the list below. \n')
+if len(argList) > 5:
+    sys.stderr.write('Error! wrong argument length, please check that you have the right command from the list below. \nreverse\ncopy\nduplicate\nreplace')
 
-match argList[1]:
-    case 'reverse':
-        with open(inputPath, 'w') as f:
-            content = f.read()
-            outputPath = argList[3]
-
-    case 'copy':
-        with open(inputPath, 'x') as f:
-            content = f.read()
-
-    case 'duplicate':
-        with open(inputPath, 'a') as f:
-            content = f.read()
-            for n in range(0, int(argList[3]):
-                f.write("\n" + content)
+if operation == 'reverse':
+    with open(inputPath, 'r') as input:
+        content = input.read()
+        with open(argList[3], 'w') as output:
+            output.write(content[::-1])
+#<<<doing something different>>>
+elif operation == 'copy':
+    shutil.copy(inputPath, argList[3])
+elif operation == 'duplicate':
+    with open(inputPath, 'r') as input:
+        content = input.read()
+        with open(inputPath, 'w') as output:
+            for n in range(0, int(argList[3])):
+                output.write("\n" + content)
+elif operation == 'replace':
+    with open(inputPath, 'r') as input:
+        content = input.read()
+        with open(inputPath, 'w') as output:
+            output.write(content.replace(argList[3], argList[4]))
+else: sys.stderr.write('Error! wrong command, please check you have the right command from the list below. \nreverse\ncopy\nduplicate\nreplace')
