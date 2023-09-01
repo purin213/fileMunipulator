dfd0bd3ad27a310fd8227490bd70084f474929be
Christian <purin213@hotmail.com>
2023-09-01 16:49:22 +0900

    added basic logic for 3 commands

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import sys
-import os
 
-path = './text.txt'
-with open(path, 'r') as f:
-    content = file.read()
-    print(content)
+argList = sys.argv
+inputPath = argList[2]
 
-with open(path, 'w') as f:
-    file.write(content + "\nAppended content from main.py")
+if len(argList) > 4:
+    sys.stderr.write('Error! wrong argument length, please check from the list below. \n')
+
+match argList[1]:
+    case 'reverse':
+        with open(inputPath, 'w') as f:
+            content = f.read()
+            outputPath = argList[3]
+
+    case 'copy':
+        with open(inputPath, 'x') as f:
+            content = f.read()
+
+    case 'duplicate':
+        with open(inputPath, 'a') as f:
+            content = f.read()
+            for n in range(0, int(argList[3]):
+                f.write("\n" + content)
